21efb840966a6377b6df326d04d57274f9297fa8
Ezra <191688309+ezrakanga@users.noreply.github.com>
2025-02-26 13:16:54 +0300

    Completed the 1st test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(a , b) {
+  let sum = 0;
 
+  let min = Math.min(a , b);
+  let max = Math.max(a , b);
+
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
