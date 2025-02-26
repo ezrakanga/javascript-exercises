3d5422bb5070d9c8686811ffb04a2221752a0192
Ezra <191688309+ezrakanga@users.noreply.github.com>
2025-02-26 14:01:00 +0300

    Completed the 5th test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,14 +1,17 @@
 const sumAll = function(a , b) {
   let sum = 0;
+  
+  let min = Math.min(a , b);  //gets the smallest number from the two parameters
+  let max = Math.max(a , b);  //gets the largest number from the two parameters
 
-  if (a < 0 || b < 0) {
+  if (
+    a < 0 || b < 0 ||  //checks to make sure the numbers are positive
+    !Number.isInteger(a) || !Number.isInteger(b)  //checks whether the number is an integer
+  ) {
     return `ERROR`;
   }
 
-  let min = Math.min(a , b);
-  let max = Math.max(a , b);
-
-  for (let i = min; i <= max; i++) {
+  for (let i = min; i <= max; i++) {  //loops through the parameters and adds each number to the previous sum
     sum += i;
   }
 
