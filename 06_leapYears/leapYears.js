7ac5fe3afb52420664429e524125d1e9b62be5ab
Ezra <191688309+ezrakanga@users.noreply.github.com>
2025-02-27 16:44:11 +0300

    Completed the 1st test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if (year % 100 === 0) {
+    return year % 400 === 0;  //if divisible by 100, it must also be divisible by 400
+  } else {
+    return year % 4 === 0;  // otherwise, it just needs to be divisible by 4
+  }
 };
 
 // Do not edit below this line
