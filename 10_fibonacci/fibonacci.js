2bd6669db42e29f21b16e7ac809f5001b3f53900
ezrakanga <191688309+ezrakanga@users.noreply.github.com>
2025-04-24 12:20:21 +0300

    convert numeric strings to numbers used the Number()

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,17 +1,20 @@
 const fibonacci = function(num) {
+  // convert numeric strings to numbers
+  let convertedNum = Number(num);
+
   // initialize the first 2 numbers and an empty sum variable
   let num1 = 0;
   let num2 = 1;
   let sum;
 
-  if (num < 0) {
+  if (convertedNum < 0) {
     return 'OOPS';
-  } else if (num === 0) {
+  } else if (convertedNum === 0) {
     return num1;
-  } else if (num === 1) {
+  } else if (convertedNum === 1) {
     return num2;
   } else {
-    for (let i = 2; i <= num; i++) {
+    for (let i = 2; i <= convertedNum; i++) {
       sum = num1 + num2;
       num1 = num2;
       num2 = sum;
